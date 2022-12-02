9d9a40b9f16be5851d9076f8112c6b66f5b7c245
sczJAIA <j.alejandro460@gmail.com>
2022-12-02 10:29:30 -0400

    render

--- src/infrastructure/controller/lead.ctrl.ts
+++ src/infrastructure/controller/lead.ctrl.ts
@@ -1,6 +1,5 @@
 import { Request, response, Response } from "express";
 import { LeadCreate } from "../../application/lead.create";
-import * as path from 'path';
 import * as fs from 'fs';
 
 class LeadCtrl {
@@ -11,10 +10,11 @@ class LeadCtrl {
     const response = await this.leadCreator.sendMessageAndSave({ message, phone })
     res.send(response);
   };
-  public sendQr = async ( res: Response) => {
-    const qr = fs.readFileSync('../../../tmp/qr.svg');
-    console.log(qr);
-    res.send({ message: 'Hola okay' });
+  public sendQr = async ({ body }: Request, res: Response) => {
+    const { message, phone } = body;
+    const path = `${process.cwd()}/tmp`;
+    const qr = fs.readFileSync(`${path}/qr.svg`);
+    res.send({ message, phone });
   };
 }
 
--- src/infrastructure/router/lead.route.ts
+++ src/infrastructure/router/lead.route.ts
@@ -8,6 +8,6 @@ const router: Router = Router();
  */
 const leadCtrl: LeadCtrl = container.get("lead.ctrl");
 router.post("/", leadCtrl.sendCtrl);
-router.get("/", leadCtrl.sendQr);
+router.post("/qr", leadCtrl.sendQr);
 
 export { router };
